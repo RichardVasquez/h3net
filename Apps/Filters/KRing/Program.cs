c996238f2845dfc06b5116d53d515f22159dfa99
Richard Vasquez <github.com@callahansplace.org>
2021-01-29 19:20:02 -0800

    Filters

---
+++ Apps/Filters/KRing/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using CommandLineParser.Arguments;
+using H3Lib;
+using H3Lib.Extensions;
+
+namespace KRing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser =
+                new CommandLineParser.CommandLineParser();
+
+            args = args.Select(s => s.ToLower()).ToArray();
+
+            try
+            {
+                var argParser = new HexRangeArguments();
+                parser.ExtractArgumentAttributes(argParser);
+                parser.ParseCommandLine(args);
+                ProcessArguments(argParser);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unable to parse input.");
+                parser.ShowUsage();
+            }
+        }
+
+        private static void ProcessArguments(HexRangeArguments argParser)
+        {
+            var radius = argParser.KRadius;
+            var origin = new H3Index(argParser.Origin);
+            var showDistance = argParser.Print;
+            
+            if (!origin.IsValid())
+            {
+                Console.WriteLine("0");
+                return;
+            }
+
+            var lookup = origin.KRingDistances(radius);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var pair in lookup)
+            {
+                sb.Clear();
+                sb.Append(pair.Key.ToString());
+                if (showDistance)
+                {
+                    sb.Append($"   {pair.Value}");
+                }
+                Console.WriteLine(sb.ToString());
+            }
+        }
+    }
+
+    public class HexRangeArguments
+    {
+        [SwitchArgument("print-distances", false, Optional = true, Description = "Print Distances")]
+        public bool Print;
+
+        [BoundedValueArgument(typeof(int), 'k', Optional = false, Description = "K Ring radius")]
+        public int KRadius;
+        
+        [BoundedValueArgument(typeof(ulong), 'o', "origin", Optional = false)]
+        public ulong Origin;
+
+    }
+
+}
---
+++ Apps/Filters/LocalIjToH3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using CommandLineParser.Arguments;
+using H3Lib;
+using H3Lib.Extensions;
+
+namespace LocalIjToH3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser =
+                new CommandLineParser.CommandLineParser();
+
+            args = args.Select(s => s.ToLower()).ToArray();
+
+            try
+            {
+                var argParser = new HexRangeArguments();
+                parser.ExtractArgumentAttributes(argParser);
+                parser.ParseCommandLine(args);
+                ProcessArguments(argParser);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unable to parse input.");
+                parser.ShowUsage();
+            }
+        }
+
+        private static void ProcessArguments(HexRangeArguments argParser)
+        {
+            var origin = new H3Index(argParser.Origin);
+
+            var ij = new CoordIj(argParser.I, argParser.J);
+
+            var (status, cell) = ij.ToH3Experimental(origin);
+
+            Console.WriteLine
+                (
+                 status != 0
+                     ? "NA"
+                     : cell.ToString()
+                );
+        }
+    }
+
+    public class HexRangeArguments
+    {
+        [BoundedValueArgument(typeof (int),'i', Optional = false, Description = "I index")]
+        public int I;
+
+        [BoundedValueArgument(typeof(int), 'j', Optional = false, Description = "J index")]
+        public int J;
+        
+        [BoundedValueArgument(typeof(ulong), 'o', "origin", Optional = false)]
+        public ulong Origin;
+
+    }
+
+}
